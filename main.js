bcba541544a3adfbf429ba1b56b3058cec989051
Saramantha <106452539+Saramantha@users.noreply.github.com>
2022-11-06 13:29:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+canvas = document.getElementById('canvas');
+ctx = canvas.getContex("2d");
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e)
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+}
+    if(( keyPRESSED >=97 && keyPressed<=122)||) (keyPressed >=65 && keyPressed<=90)
+    {
+        aplhabetkey():
+        document.getElementById("d1").innerHTML"Você apertou uma tecla alfabeto";
+        console.log("alphabet key");
+    }
+    function alphabetkey()
+    {
+        img_image="alfabeto.png";
+        addEventListener();
+    }
